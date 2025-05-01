556a80f766c34d41755ee3358f2e092eeb815e3b
Manolis <sinadinakis.e@gmail.com>
2025-05-01 20:15:30 +0300

    feat: implement signup form with email, password, and subscription fields

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <div class="min-h-screen flex items-center justify-center bg-gray-100">
+            <UserLogin />
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import '@provetcloud/css';
+    import UserLogin from '~/pages/login/UserLogin.vue';
+</script>
---
+++ pages/login/UserLogin.vue
@@ -0,0 +1,55 @@
+<template>
+    <provet-card class="flex justify-center max-w-5xl mx-auto">
+        <form @submit.prevent="handleSubmit">
+            <div class="flex flex-col gap-4 sm:gap-2">
+                <provet-input
+                    v-model="email"
+                    id="email"
+                    type="email"
+                    label="Email"
+                />
+                <div class="flex flex-col gap-4 sm:gap-2">
+                    <div class="flex flex-col sm:flex-row gap-4 sm:gap-2">
+                        <provet-input
+                            v-model="password"
+                            id="password"
+                            :type="showPassword ? 'text' : 'password'"
+                            label="Password"
+                            @click="togglePassword"
+                        />
+                    </div>
+                </div>
+                <provet-button
+                    variant="primary"
+                    @click="handleSubmit"
+                >
+                    <provet-spinner v-if="loading" size="xl"></provet-spinner>
+                    <span v-else>Sign Up</span>
+                </provet-button>
+            </div>
+        </form>
+    </provet-card>
+</template>
+
+<script lang="ts" setup>
+    import '@provetcloud/css';
+
+    const router = useRouter();
+
+    const email = ref('');
+    const password = ref('');
+    const showPassword = ref(false);
+    const submitted = ref(false);
+    const loading = ref(false);
+
+    function togglePassword() {
+        showPassword.value = !showPassword.value;
+    }
+
+    async function handleSubmit() {
+        submitted.value = true;
+
+        loading.value = true;
+        await router.push('/success');
+    }
+</script>
