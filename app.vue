3312dfecf5d20a5b33ce0943a7eb5f25961e93a6
Manolis <sinadinakis.e@gmail.com>
2025-05-01 20:09:20 +0300

    chore: initialize Nuxt 3 project with client-side rendering

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+    <NuxtLayout name="default">
+        <NuxtPage />
+    </NuxtLayout>
+</template>
+
+<script setup>
+
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  ssr: false, // Client-side rendering only
+  css: ['~/assets/css/main.css'],
+  modules: [
+    '@nuxt/test-utils/module',
+    '@pinia/nuxt',
+    '@nuxt/fonts',
+    '@vueuse/nuxt'
+  ],
+  pinia: {
+    storesDirs: ['./stores/**', './custom-folder/stores/**'],
+  },
+  devtools: { enabled: true },
+  vite: {
+    plugins: [ tailwindcss() ],
+    vue: {
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.includes("-"),
+        },
+      },
+    },
+  }
+})
